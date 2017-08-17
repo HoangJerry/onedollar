531d67df9b16c6e386340af1f24bb7c501ec7c4d
HoangTN <HoangTN@gmail.com>
2017-08-17 18:31:15 +0700

    fix re

--- onedollar_backend/settings_local.py
+++ onedollar_backend/settings_local.py
@@ -7,33 +7,33 @@ DATABASES = {
         # The following settings are not used with sqlite3:
         'USER': 'root',
         'PASSWORD': 'softdev',
-        'HOST': 'localhost',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '3306',                      # Set to empty string for default.
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
     }
 }
 
 #SITE_URL = 'http://128.199.67.33/'
-SITE_URL = 'http://localhost:8000/'
+SITE_URL = 'https://dailywashbackend.herokuapp.com/'
 STATIC_URL = SITE_URL + 'static/'
 MEDIA_URL = SITE_URL + 'media/'
 
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': '/var/www/html/HoangTN/project/merchant-wish-django/cron_error.log',
-            # 'filename': '/var/www/html/prs/2017/merchant-wish-django/cron_error.log',
-            },
-        },
-    'loggers': {
-        'django_crontab': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'handlers': {
+#         'file': {
+#             'level': 'DEBUG',
+#             'class': 'logging.FileHandler',
+#             'filename': '/var/www/html/HoangTN/project/merchant-wish-django/cron_error.log',
+#             # 'filename': '/var/www/html/prs/2017/merchant-wish-django/cron_error.log',
+#             },
+#         },
+#     'loggers': {
+#         'django_crontab': {
+#             'handlers': ['file'],
+#             'level': 'DEBUG',
+#             'propagate': True,
+#         },
+#     },
+# }
