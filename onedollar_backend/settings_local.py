f5a0211858d4ddf51e5ba8426d2f1d2c773f0cdd
HoangTN <HoangTN@gmail.com>
2017-08-18 08:42:28 +0700

    fix re

--- onedollar_backend/settings_local.py
+++ onedollar_backend/settings_local.py
@@ -16,9 +16,9 @@ DATABASES = {
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 # #SITE_URL = 'http://128.199.67.33/'
-# SITE_URL = 'https://dailywashbackend.herokuapp.com/'
-# STATIC_URL = SITE_URL + 'static/'
-# MEDIA_URL = SITE_URL + 'media/'
+SITE_URL = 'https://dailywashbackend.herokuapp.com/'
+STATIC_URL = SITE_URL + 'static/'
+MEDIA_URL = SITE_URL + 'media/'
 
 
 # LOGGING = {
